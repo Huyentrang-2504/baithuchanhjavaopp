ff61b50b2f4575c149ceaf4b968be205c8507597
Đinh Thị Huyền Trang <97714610+Huyentrang-2504@users.noreply.github.com>
2022-04-30 16:49:26 +0700

    Add files via upload

---
+++ Bai Thuc Hanh 2904/src/Shape/App2604.java
@@ -0,0 +1,11 @@
+package Shape;
+
+public class App2604 {
+    public static void main(String[] args) throws Exception {
+        Point a=new Point();
+        a.getTungDo();
+        a.getHoanhDo();
+        a.inDiem();
+        a.nhapDiem();
+    }
+}
